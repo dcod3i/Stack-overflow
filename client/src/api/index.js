68bd3f326b72beccb1eb393d587c0eeda28d6904
dcod3i <pavankumarck.scs20@rvce.edu.in>
2023-09-26 20:18:32 +0530

    modified API file in client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: "http://localhost:5432"})
+const API = axios.create({ baseURL: "https://stack-overflow-3uhl.onrender.com"})
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
